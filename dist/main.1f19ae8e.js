f8512b8f02e77da036fe810debe6060aa7767417
alexiabanos <alexiabanos@gmail.com>
2024-04-16 16:44:10 -0500

    Optimize mobile version for content, size and positions.

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -266,7 +266,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52445" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55585" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
