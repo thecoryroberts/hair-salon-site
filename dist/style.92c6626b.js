1885d5789228d2d52213250778eacf1d1d71447e
Cory Roberts <mail@coryroberts.net>
2024-04-21 23:21:31 -0400

    First Commit

--- dist/style.92c6626b.js
+++ dist/style.92c6626b.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"/Users/alexiabanos/Documents/GitHub/hair-salon-site/assets/fonts/icomoon.eot":[["icomoon.592762f7.eot","assets/fonts/icomoon.eot"],"assets/fonts/icomoon.eot"],"/Users/alexiabanos/Documents/GitHub/hair-salon-site/assets/fonts/icomoon.ttf":[["icomoon.31a6ec37.ttf","assets/fonts/icomoon.ttf"],"assets/fonts/icomoon.ttf"],"/Users/alexiabanos/Documents/GitHub/hair-salon-site/assets/fonts/icomoon.woff":[["icomoon.4d39a651.woff","assets/fonts/icomoon.woff"],"assets/fonts/icomoon.woff"],"/Users/alexiabanos/Documents/GitHub/hair-salon-site/assets/fonts/icomoon.svg":[["icomoon.e5b6a6c2.svg","assets/fonts/icomoon.svg"],"assets/fonts/icomoon.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/home/corbo/src/hair-salon-site/assets/fonts/icomoon.eot":[["icomoon.592762f7.eot","assets/fonts/icomoon.eot"],"assets/fonts/icomoon.eot"],"/home/corbo/src/hair-salon-site/assets/fonts/icomoon.ttf":[["icomoon.31a6ec37.ttf","assets/fonts/icomoon.ttf"],"assets/fonts/icomoon.ttf"],"/home/corbo/src/hair-salon-site/assets/fonts/icomoon.woff":[["icomoon.4d39a651.woff","assets/fonts/icomoon.woff"],"assets/fonts/icomoon.woff"],"/home/corbo/src/hair-salon-site/assets/fonts/icomoon.svg":[["icomoon.e5b6a6c2.svg","assets/fonts/icomoon.svg"],"assets/fonts/icomoon.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57624" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34455" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
